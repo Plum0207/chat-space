01f2a8052184fef90e426d9ccaf0f7180b66c7fc
Plum0207 <lapine.blanche.27@gmail.com>
2019-08-31 12:13:24 +0900

    Revert "Edit "messages_controller.rb""
    This reverts commit d9fcd8acbd1760a1df432eefc4a0dba9561365ef.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,17 +8,12 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    binding.pry
-    respond_to do |format|
-      format.html
-        if @message.save
-          redirect_to group_messages_path(@group), notice: "メッセージが投稿されました"
-        else
-          @messages = @group.messages.includes(:user)
-          flash.now[:alert] = "メッセージを入力してください"
-          render :index
-        end
-      format.json
+    if @message.save
+      redirect_to group_messages_path(@group), notice: "メッセージが投稿されました"
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = "メッセージを入力してください"
+      render :index
     end
   end
 
