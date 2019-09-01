da02d5a717de3cdfee69a683e57598c4c08081b4
Plum0207 <lapine.blanche.27@gmail.com>
2019-09-01 13:24:06 +0900

    Request Ajax
    - Edit "serch.js"
    	- Add Ajax request
    - Modify ""users_controller.rb""

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,12 @@
 $(function(){
   $("#user-search-field").on("keyup",function(){
     let input = $("#user-search-field").val();
-    console.log(input);
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { search: input },
+      dataType: 'json'
+    })
   });
 });
