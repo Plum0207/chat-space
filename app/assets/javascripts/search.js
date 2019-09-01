3a076c18d69c15d733eaf8c6808eab2f667652d9
Plum0207 <lapine.blanche.27@gmail.com>
2019-09-01 15:07:36 +0900

    Update search.js
    - edit conditional branch by users existence
    	- add else
    - modify misspelling of "length"
    - define "function appendErrMsgToHTML"
    	- define variable "html"
    	- append html to parent class

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -5,10 +5,17 @@ function appendUser(user){
                 <p class="chat-group-user__name">${ user.name }</p>
                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
               </div>`
-  $("#user-search-result").append(html)
+  $("#user-search-result").append(html);
 }
 
-  $("#user-search-field").on("keyup",function(){
+function appendErrMsgToHTML(msg){
+  let html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${ msg }</p>
+              </div>`
+  $("#user-search-result").append(html);
+}
+
+$("#user-search-field").on("keyup",function(){
     let input = $("#user-search-field").val();
 
     $.ajax({
@@ -19,10 +26,13 @@ function appendUser(user){
     })
     .done(function(users){
       $("#user-search-result").empty();
-      if(users.lenght !==0) {
+      if(users.length !==0) {
         users.forEach(function(user){
           appendUser(user);
         });
+      } 
+      else {
+        appendErrMsgToHTML("一致するユーザーが見つかりません");
       }
     })
   });
