3b0f51c5666cf8772a7c6cd25570ef0427ed1cf7
Plum0207 <lapine.blanche.27@gmail.com>
2019-09-01 14:30:15 +0900

    Update search.js
    - add done(function(users){})
    - conditional branch by users existence
    - define "funciton appedUser"
    	- define variable "html"
    	- append html to parent class

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,13 @@
 $(function(){
+
+function appendUser(user){
+  let html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${ user.name }</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+              </div>`
+  $("#user-search-result").append(html)
+}
+
   $("#user-search-field").on("keyup",function(){
     let input = $("#user-search-field").val();
 
@@ -7,6 +16,14 @@ $(function(){
       url: '/users',
       data: { search: input },
       dataType: 'json'
+    })
+    .done(function(users){
+      $("#user-search-result").empty();
+      if(users.lenght !==0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
     })
   });
 });
