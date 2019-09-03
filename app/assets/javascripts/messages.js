b6c3d2aeea8e8cdc00daee6b6d30b967c0ab65c9
Plum0207 <lapine.blanche.27@gmail.com>
2019-09-03 23:52:58 +0900

    Revert "Edit messages.js"
    This reverts commit f7c869e3ce71377eb06b6bfbb7c2aad0750e5d50.

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildHTML(message){
-    let htmlUpperInfo = `<div class="message" data-message_id="${message.id}">
+    let htmlUpperInfo = `<div class="message">
                           <div class="message__upper-info">
                             <div class="message__upper-info__talker">
                             ${message.user_name}
@@ -73,11 +73,11 @@ $(function(){
       data: { id: last_message_id },
       dataType: 'json',
     })
-    .done(function(messages){
+    .done(funciton(messages){
 
     })
-    .fail(function(){
-      console.log('error');
+    .fail(funciton(){
+
     });
   };
 });
